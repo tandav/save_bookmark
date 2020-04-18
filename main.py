1b71acc5314c3d1232da34e1ecd312ba9f729569
Alexander Rodionov <alx1dr@gmail.com>
2020-04-18 16:45:24 +0300

    _

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import subprocess
-import tkinter.filedialog
 from pathlib import Path
 import plistlib
+
+def clean_title(title):
+    return ''.join(x if x.isalnum() else '_' for x in title)
 
 
 js_path = str(Path(__file__).absolute().parent / 'chrome_tabs.js')
@@ -11,6 +13,8 @@ p = subprocess.run(cmd, text=True, capture_output=True)
 
 if p.returncode == 0:
     url, title, folder = p.stderr.splitlines()
+
+    title = clean_title(title)
 
     with (Path(folder) / title).with_suffix('.webloc').open('wb') as f:
         plistlib.dump({'URL': url}, f)
